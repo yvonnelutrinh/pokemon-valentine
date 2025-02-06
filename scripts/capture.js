869b9b37491b5ef393037d8b0d3a3c369b4577fc
jiinp2 <jiinpark.work@gmail.com>
2025-02-06 13:02:06 -0500

    pnd download and h2

---
+++ scripts/capture.js
@@ -0,0 +1,12 @@
+document
+  .getElementById("download-button")
+  .addEventListener("click", function () {
+    const section = document.getElementById("download");
+
+    html2canvas(section).then((canvas) => {
+      const link = document.createElement("a");
+      link.href = canvas.toDataURL("image/png");
+      link.download = "pokemon-valentine.png";
+      link.click();
+    });
+  });
